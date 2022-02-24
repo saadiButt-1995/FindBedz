7bde262d908b00c570db9fa207ce47fa58d1f0b4
saadiButt-1995 <saadmb1995@gmail.com>
2022-02-24 21:14:29 +0500

    thursday

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import Axios from "axios"
 
-// const basePath = "https://shelterprovider.herokuapp.com/v1/"
-const basePath = "http://192.168.100.179:5000/v1/"
+const basePath = "https://shelterprovider.herokuapp.com/v1/"
+// const basePath = "http://192.168.100.179:5000/v1/"
 Axios.defaults.baseURL = basePath
 Axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
 
