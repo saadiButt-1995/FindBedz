2282b620cf9abc09a58ee4715f23d06b56686d1a
saadiButt-1995 <saadmb1995@gmail.com>
2022-04-19 16:55:01 +0500

    little bit changes

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
-import Axios from "axios"
+import Axios from "axios";
 
-const basePath = "https://find-bedz.herokuapp.com/v1/"
+const basePath = "https://find-bedz.herokuapp.com/v1/";
 // const basePath = "http://192.168.100.179:5000/v1/"
 
-const SocketPath = "ws://find-bedz.herokuapp.com/"
+const SocketPath = "wss://find-bedz.herokuapp.com/";
 // const SocketPath = "ws://192.168.100.179:5000/"
 
-Axios.defaults.baseURL = basePath
-Axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+Axios.defaults.baseURL = basePath;
+Axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem(
+  "token"
+)}`;
 
-const axios = Axios
+const axios = Axios;
 
-const MapKey = "AIzaSyC_LWPOTamnTLKR0IVa5pX_w2Zxo9hE0Sw"
+const MapKey = "AIzaSyC_LWPOTamnTLKR0IVa5pX_w2Zxo9hE0Sw";
 
-export {axios, basePath, MapKey, SocketPath}
+export { axios, basePath, MapKey, SocketPath };
